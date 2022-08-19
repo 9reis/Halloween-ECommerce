cc18634b184c0b7d7bdf36a35c5f26bc1e02a519
Lucas Reis <lucasreis_9@hotmail.com>
2022-08-18 21:51:51 -0300

    Finalizada a logica scroll spy

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ document.querySelector('#search-btn').onclick = () => {
     navbar.classList.remove('active');
 }
 
+//scroll spy 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.header .navbar a');
+
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
@@ -21,6 +25,20 @@ window.onscroll = () => {
     }else{
         document.querySelector('.header').classList.remove('active');
     }
+
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 200;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height ){
+            navLinks.forEach(link => {
+              link.classList.remove('active');
+              document.querySelector('.header .navbar a[href*='+id+']').classList.add('active')
+          });
+        }
+    } );
 }
 
 window.onload = () => {
